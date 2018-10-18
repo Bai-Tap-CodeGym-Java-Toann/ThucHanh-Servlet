7d0621d2a3c60ebab10a71de1519216c0d2190a8
Nguyễn Đại Toàn <sitdownrightnow2552@gmail.com>
2018-10-18 11:15:56 +0700

    fin.

--- src/ServerTime.java
+++ src/ServerTime.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
-@WebServlet(name = "Servlet", urlPatterns = "/time")
+@WebServlet(name = "Servlet",urlPatterns = "/timer")
 public class ServerTime extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
